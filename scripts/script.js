8a7e883caeaa4b9098d9dadfdd5ce83c40b5fd58
Kerem Tutucu <Keremtutucu@gmail.com>
2024-09-04 13:00:22 +0200

    aanpassingen
    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
-
-function expand () {
-  document.body.classList.toggle('expand')
+document.getElementById('contactButton').onclick = function() {
+  var details = document.getElementById('contactDetails');
+  if (details.style.display === "none") {
+      details.style.display = "block";
+      this.textContent = "Verberg Contactgegevens";
+  } else {
+      details.style.display = "none";
+      this.textContent = "Bekijk Contactgegevens";
+  }
 }
